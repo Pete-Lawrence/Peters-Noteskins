b577397f97a98215a6a750c571ec26105ed3c390
Pete-Lawrence <petergover199x@gmail.com>
2023-04-06 20:05:09 +0100

    update to fonts

---
+++ SM5/Simply-Love-Assets/Fonts/Instances of WENDY/BGAnimations/ScreenEvaluationStage in/default.lua
@@ -0,0 +1,107 @@
+-- assume that all human players failed
+local failed = true
+SL.Global.Restarts = 0
+
+-- loop through all available human players
+for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+	-- if any of them passed, we want to display the "cleared" graphic
+	if not STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetFailed() then
+		failed = false
+	end
+end
+
+if ThemePrefs.Get("VisualStyle") ~= "SRPG6" then
+	local img = failed and "failed text (doubleres).png" or "cleared text (doubleres).png"
+
+	return Def.ActorFrame {
+		Def.Quad{
+			InitCommand=function(self) self:FullScreen():diffuse(Color.Black) end,
+			OnCommand=function(self) self:sleep(0.2):linear(0.5):diffusealpha(0) end,
+		},
+
+		LoadActor(img)..{
+			InitCommand=function(self) self:Center():zoom(0.8):diffusealpha(0) end,
+			OnCommand=function(self) self:accelerate(0.4):diffusealpha(1):sleep(0.6):decelerate(0.4):diffusealpha(0) end
+		}
+	}
+else
+	local af = Def.ActorFrame {
+		InitCommand=function(self)
+			self:xy(_screen.cx, _screen.cy)
+		end,
+		Def.Quad{
+			InitCommand=function(self)
+				self:zoomto(_screen.w,  100):diffuse(Color.Black):diffusealpha(0):fadetop(0.2):fadebottom(0.2)
+			end,
+			OnCommand=function(self)
+				self:linear(0.25):diffusealpha(0.7):sleep(2):linear(0.25):diffusealpha(0)
+			end,
+		},
+	}
+
+	if failed then
+		af[#af+1] = Def.Sprite {
+			Texture=THEME:GetPathG("", "_VisualStyles/SRPG6/YouDied.png"),
+			InitCommand=function(self) self:zoom(0.36):diffusealpha(0) end,
+			OnCommand=function(self)
+				self:linear(0.25):diffusealpha(1):linear(2):zoom(0.38):linear(0.25):diffusealpha(0):zoom(0.39)
+				SOUND:PlayOnce(THEME:GetPathS("", "SRPG6-YouDied.ogg"))
+			end
+		}
+	else
+		local image = THEME:GetPathG("", "_VisualStyles/SRPG6/EnemyFelled.png")
+
+		local bosses = {
+			["e25513cb3c801604"]="LegendFelled.png",
+			["be1811d125b4b9d5"]="LegendFelled.png",
+			["945ec467c0b8fd94"]="LegendFelled.png",
+			["e410d5bf872d5f37"]="LegendFelled.png",
+			["e61476eec77277ca"]="LegendFelled.png",
+			["21a111709f4416b3"]="LegendFelled.png",
+			["ccb5ff37f938b057"]="LegendFelled.png",
+			["65910d53c611f328"]="LegendFelled.png",
+			["12031d8f99c8b88c"]="LegendFelled.png",
+			["24a05523a0131b20"]="LegendFelled.png",
+			["24a05523a0131b20"]="LegendFelled.png",
+
+			["2184afe998acc7a5"]="GodSlain.png",
+			["bd80236dc1de432f"]="GodSlain.png",
+			["f56588cee23985ed"]="GodSlain.png",
+			["eb20a2c5f6674882"]="GodSlain.png",
+		}
+
+		local found_boss = false
+		for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+			local pn = ToEnumShortString(player)
+			local chartHash = SL[pn].Streams.Hash
+			if bosses[chartHash] ~= nil then
+				image = THEME:GetPathG("", "_VisualStyles/SRPG6/"..bosses[chartHash])
+				found_boss = true
+				break
+			end
+		end
+
+		if not found_boss and not GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentSong():GetLastSecond() > 16 * 60 then
+			image = THEME:GetPathG("", "_VisualStyles/SRPG6/GreatEnemyFelled.png")
+		end
+
+		af[#af+1] = Def.Sprite {
+			Texture=image,
+			InitCommand=function(self) self:zoomx(0.4):zoomy(0.38):diffusealpha(0) end,
+			OnCommand=function(self)
+				self:linear(0.25):diffusealpha(0.15):decelerate(2):zoomx(0.44):linear(0.25):diffusealpha(0)
+				SOUND:PlayOnce(THEME:GetPathS("", "SRPG6-EnemyFelled.ogg"))
+			end
+		}
+
+		af[#af+1] = Def.Sprite {
+			Texture=image,
+			InitCommand=function(self) self:zoom(0.38):diffusealpha(0) end,
+			OnCommand=function(self)
+				self:linear(0.25):diffusealpha(1):linear(2):linear(0.25):diffusealpha(0)
+			end
+		}
+	end
+
+	return af
+end
---
+++ SM5/Simply-Love-Assets/Fonts/Instances of WENDY/BGAnimations/ScreenGameOver overlay/default.lua
@@ -0,0 +1,96 @@
+local Players = GAMESTATE:GetHumanPlayers();
+
+local t = Def.ActorFrame{
+	LoadFont("Mega/_mega")..{
+		Text="GAME",
+		InitCommand=function(self) self:xy(_screen.cx,_screen.cy-40):croptop(1):fadetop(1):zoom(1.2):shadowlength(1) end,
+		OnCommand=function(self) self:decelerate(0.5):croptop(0):fadetop(0):glow(1,1,1,1):decelerate(1):glow(1,1,1,1) end,
+		OffCommand=function(self) self:accelerate(0.5):fadeleft(1):cropleft(1) end
+	},
+	LoadFont("Mega/_mega")..{
+		Text="OVER",
+		InitCommand=function(self) self:xy(_screen.cx,_screen.cy+40):croptop(1):fadetop(1):zoom(1.2):shadowlength(1) end,
+		OnCommand=function(self) self:decelerate(0.5):croptop(0):fadetop(0):glow(1,1,1,1):decelerate(1):glow(1,1,1,1) end,
+		OffCommand=function(self) self:accelerate(0.5):fadeleft(1):cropleft(1) end
+	},
+
+	--Player 1 Stats BG
+	Def.Quad{
+		InitCommand=function(self)
+			self:zoomto(160,_screen.h):xy(80, _screen.h/2):diffuse(color("#00000099"))
+			if ThemePrefs.Get("RainbowMode") then self:diffuse(color("#000000dd")) end
+		end,
+	},
+
+	--Player 2 Stats BG
+	Def.Quad{
+		InitCommand=function(self)
+			self:zoomto(160,_screen.h):xy(_screen.w-80, _screen.h/2):diffuse(color("#00000099"))
+			if ThemePrefs.Get("RainbowMode") then self:diffuse(color("#000000dd")) end
+		end,
+	}
+}
+
+local line_height = 58
+local profilestats_y = 138
+local horiz_line_y   = 288
+local normalstats_y  = 268
+
+for player in ivalues(Players) do
+
+	local stats
+	local x_pos = player==PLAYER_1 and 80 or _screen.w-80
+	local PlayerStatsAF = Def.ActorFrame{ Name="PlayerStatsAF_"..ToEnumShortString(player) }
+
+
+	-- first, check if this player is using a profile (local or MemoryCard)
+	if PROFILEMAN:IsPersistentProfile(player) then
+
+		-- if a profile is in use, grab gameplay stats for this session that are pertinent
+		-- to this specific player's profile (highscore name, calories burned, total songs played)
+		local profile_stats = LoadActor("PlayerStatsWithProfile.lua", player)
+
+		-- loop through those stats, adding them to the ActorFrame for this player as BitmapText actors
+		for i,stat in ipairs(profile_stats) do
+			PlayerStatsAF[#PlayerStatsAF+1] = LoadFont("Common Normal")..{
+				Text=stat,
+				InitCommand=function(self)
+					self:diffuse(PlayerColor(player)):zoom(0.95)
+						:xy(x_pos, (line_height*(i-1)) + profilestats_y)
+						:maxwidth(150):vertspacing(-1)
+
+					DiffuseEmojis(self)
+				end
+			}
+		end
+
+		PlayerStatsAF[#PlayerStatsAF+1] = LoadActor("./ProfileAvatar", {player, x_pos})
+	end
+
+	-- horizontal line separating upper stats (profile) from the lower stats (general)
+	PlayerStatsAF[#PlayerStatsAF+1] = Def.Quad{
+		InitCommand=function(self)
+			self:zoomto(120,1):xy(x_pos, horiz_line_y)
+				:diffuse( PlayerColor(player) )
+		end
+	}
+
+	-- retrieve general gameplay session stats for which a profile is not needed
+	stats = LoadActor("PlayerStatsWithoutProfile.lua", player)
+
+	-- loop through those stats, adding them to the ActorFrame for this player as BitmapText actors
+	for i,stat in ipairs(stats) do
+		PlayerStatsAF[#PlayerStatsAF+1] = LoadFont("Common Normal")..{
+			Text=stat,
+			InitCommand=function(self)
+				self:diffuse(PlayerColor(player)):zoom(0.95)
+					:xy(x_pos, (line_height*i) + normalstats_y)
+					:maxwidth(150):vertspacing(-1)
+			end
+		}
+	end
+
+	t[#t+1] = PlayerStatsAF
+end
+
+return t
